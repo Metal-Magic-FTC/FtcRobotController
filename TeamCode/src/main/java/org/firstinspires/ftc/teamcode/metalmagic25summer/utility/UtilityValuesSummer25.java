084b29803c43b3da0046f400a998a05fbbbe6790
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2025-05-21 13:53:31 -0400

    Summer 2025 v0.0.1
    Getting the repo ready.
    Creating packages and UtilityValues for the offseason

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/metalmagic25summer/utility/UtilityValuesSummer25.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.metalmagic25summer.utility;
+
+/**
+ * Insert all universal constants here.
+ * Ex: motor direction, offsets, slide heights, etc
+ * This file is only to be used during the summer of 2025 during off-season
+ *
+ * To call any of these values, import this file in the code
+ * String myString = UtilityValuesSummer25.TEST_EXAMPLE_MESSAGE;
+ *
+ * Conventions in this file:
+ * variables must start with 'public static final' before inserting type, name and value.
+ * Variable names should be all capitalized with underscores as spaces.
+ * Example"
+ * public static final String TEST_EXAMPLE_MESSAGE =  "This is a test example message!";
+ *
+ */
+public class UtilityValuesSummer25 {
+
+    public static final String TEST_EXAMPLE_MESSAGE =  "This is a test example message!";
+
+}
