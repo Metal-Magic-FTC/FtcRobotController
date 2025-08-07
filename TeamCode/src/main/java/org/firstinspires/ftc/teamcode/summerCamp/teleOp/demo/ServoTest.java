fb442bd9b01d68422151e620d9e47cbc17f01518
aryanmathur1 <134450794+aryanmathur1@users.noreply.github.com>
2025-08-07 10:09:03 -0400

    Create ServoTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summerCamp/teleOp/demo/ServoTest.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.summerCamp.teleOp.demo;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.mmintothedeep.UtilityValues;
+
+@TeleOp(name="Summer Camp Servo Test")
+public class ServoTest extends LinearOpMode {
+
+    DcMotor leftFrontDrive = null;
+    DcMotor rightFrontDrive = null;
+    DcMotor leftBackDrive = null;
+    DcMotor rightBackDrive = null;
+
+    Servo pivotServo = null;
+    Servo clawServo = null;
+
+    /**
+     * Main section of code -- like 'main' method
+     * @throws InterruptedException - just in case
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize(); // initializing everything
+
+        waitForStart(); // waiting until driver clicks play button
+
+        while (opModeIsActive()) {
+
+            double clawServoPosition = gamepad1.right_trigger;
+
+            clawServo.setPosition(clawServoPosition);
+
+            double pivotServoPosition = gamepad1.left_trigger;
+
+            clawServo.setPosition(pivotServoPosition);
+
+            telemetry.addData("Claw", clawServo.getPosition());
+            telemetry.addData("Pivot", pivotServo.getPosition());
+            telemetry.update();
+
+        }
+
+    }
+
+    /**
+     * code to initialize everything
+     */
+    public void initialize() {
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftBackDrive.setDirection(UtilityValues.compLeftBackDirection);
+        leftFrontDrive.setDirection(UtilityValues.compLeftFrontDirection);
+        rightBackDrive.setDirection(UtilityValues.compRightBackDirection);
+        rightFrontDrive.setDirection(UtilityValues.compRightFrontDirection);
+
+        pivotServo = hardwareMap.servo.get("pivotServo");
+        clawServo = hardwareMap.servo.get("clawServo");
+
+    }
+
+
+}
