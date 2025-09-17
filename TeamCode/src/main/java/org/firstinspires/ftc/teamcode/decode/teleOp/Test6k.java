5af45ebef24131d7641dbc4cccc5871a6ed684b7
Aryan Mathur <134450794+aryanmathur1@users.noreply.github.com>
2025-09-17 19:43:59 -0400

    Update Test6k.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/Test6k.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/Test6k.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name="!Test 6k rpm")
 public class Test6k extends LinearOpMode {
 
@@ -18,19 +20,55 @@ public class Test6k extends LinearOpMode {
 
         waitForStart(); // waiting until driver clicks play button
 
+        double speed = 1.0;
+
+        boolean upWasPressed = false;
+        boolean downWasPressed = false;
+
+        boolean upPressed = false;
+        boolean downPressed = false;
+
         while (opModeIsActive()) {
 
-
             if (gamepad1.a) {
-                leftLaunch.setPower(1);
-                rightLaunch.setPower(1);
+                leftLaunch.setPower(speed);
+                rightLaunch.setPower(speed);
             } else if (gamepad1.b) {
-                leftLaunch.setPower(-1);
-                rightLaunch.setPower(-1);
+                leftLaunch.setPower(-1*speed);
+                rightLaunch.setPower(-1*speed);
             } else {
                 leftLaunch.setPower(0);
                 rightLaunch.setPower(0);
             }
+
+//            if (gamepad1.dpad_up) {
+//                speed += 0.00001;
+//            }
+//
+//            if (gamepad1.dpad_down) {
+//                speed -= 0.00001;
+//            }
+
+            upPressed = (gamepad1.dpad_up);
+            downPressed = (gamepad1.dpad_down);
+
+            if (upPressed && !upWasPressed) {
+                speed += 0.01;
+            }
+
+            if (downPressed && !downWasPressed) {
+                speed -= 0.01;
+            }
+
+            upWasPressed = (gamepad1.dpad_up);
+            downWasPressed = (gamepad1.dpad_down);
+
+
+            if (speed < 0) { speed = 0; }
+            if (speed > 1) { speed = 1; }
+
+            telemetry.addData("speed", speed);
+            telemetry.update();
 
         }
     }
@@ -44,7 +82,7 @@ public class Test6k extends LinearOpMode {
         rightLaunch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         leftLaunch.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightLaunch.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightLaunch.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
