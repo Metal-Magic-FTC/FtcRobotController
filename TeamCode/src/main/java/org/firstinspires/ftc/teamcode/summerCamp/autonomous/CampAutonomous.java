8333417cc56f66620ced054cb70da928102ac319
Nik <robo.nik10@gmail.com>
2025-11-08 12:01:10 -0500

    creating camp code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summerCamp/autonomous/CampAutonomous.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode.summerCamp.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.mmintothedeep.UtilityValues;
+
+@Autonomous(name="TEAM 1 - Autonomous run this")
+public class CampAutonomous extends LinearOpMode {
+
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    Servo pivotServo = null;
+    Servo clawServo = null;
+
+    static final double MOTOR_TICK_COUNTS = UtilityValues.motorTicks; // goBILDA 5203 series Yellow Jacket
+    // figure out how many times we need to turn the wheels to go a certain distance
+    // the distance you drive with one turn of the wheel is the circumference of the wheel
+    // The wheel's Diameter is 96mm. To convert mm to inches, divide by 25.4
+    static final double WHEEL_DIAMETER_INCHES = UtilityValues.wheelDiameter / 25.4; // in Inches
+    static final double CIRCUMFERENCE_INCHES = Math.PI * WHEEL_DIAMETER_INCHES; // pi * the diameter of the wheels in inches
+
+    static final double DEGREES_MOTOR_MOVES_IN_1_REV = 45.0;
+
+    static final double SPEED = UtilityValues.SPEED; // Motor Power setting
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+        waitForStart();
+
+        /*
+         * ============================
+         * THIS IS THE ACTUAL DRIVING
+         * ============================
+         */
+
+        //drive code...
+        moveStraightLine(12.5);
+        strafe(-5);
+        rotate(90);
+
+    }
+
+    public void initialize() {
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftBackDrive.setDirection(UtilityValues.compLeftBackDirection);
+        leftFrontDrive.setDirection(UtilityValues.compLeftFrontDirection);
+        rightBackDrive.setDirection(UtilityValues.compRightBackDirection);
+        rightFrontDrive.setDirection(UtilityValues.compRightFrontDirection);
+
+        pivotServo = hardwareMap.servo.get("pivotServo");
+        clawServo = hardwareMap.servo.get("clawServo");
+
+    }
+
+    public void drive(double speed, double leftFrontRevs, double leftBackRevs, double rightFrontRevs, double rightBackRevs) {
+
+        int LFdrivetarget = (int) (leftFrontRevs * MOTOR_TICK_COUNTS) + leftFrontDrive.getCurrentPosition();
+        int LBdrivetarget = (int) (leftBackRevs * MOTOR_TICK_COUNTS) + leftBackDrive.getCurrentPosition();
+        int RFdrivetarget = (int) (rightFrontRevs * MOTOR_TICK_COUNTS) + rightFrontDrive.getCurrentPosition();
+        int RBdrivetarget = (int) (rightBackRevs * MOTOR_TICK_COUNTS) +  rightBackDrive.getCurrentPosition();
+
+        leftFrontDrive.setTargetPosition(LFdrivetarget);
+        leftBackDrive.setTargetPosition(LBdrivetarget);
+        rightFrontDrive.setTargetPosition(RFdrivetarget);
+        rightBackDrive.setTargetPosition(RBdrivetarget);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        leftFrontDrive.setPower(speed);
+        leftBackDrive.setPower(speed);
+        rightFrontDrive.setPower(speed);
+        rightBackDrive.setPower(speed);
+
+        while (leftFrontDrive.isBusy() || leftBackDrive.isBusy() || rightFrontDrive.isBusy() || rightBackDrive.isBusy()) {
+            telemetry.addLine("Current Position of the Motors")
+                    .addData("Left Front  ", "%d", leftFrontDrive.getCurrentPosition())
+                    .addData("Left Back ", "%d", leftBackDrive.getCurrentPosition())
+                    .addData("Right Front ", "%d", rightFrontDrive.getCurrentPosition())
+                    .addData("Right Back ", "%df", rightBackDrive.getCurrentPosition());
+
+            telemetry.addLine("Target Positions of the Motors")
+                    .addData("Left Front  ", "%d", LFdrivetarget)
+                    .addData("Left Back ", "%d", LBdrivetarget)
+                    .addData("Right Front ", "%d", RFdrivetarget)
+                    .addData("Right Back ", "%df", RBdrivetarget);
+
+            telemetry.update();
+        }
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+
+
+        sleep(250);
+    }
+
+    private void moveStraightLine(double movementInInches) {
+        double moveInRevs = movementInInches / CIRCUMFERENCE_INCHES;
+        telemetry.addData("Moving ", "%.3f inches", movementInInches);
+        telemetry.update();
+        drive(SPEED, moveInRevs, moveInRevs, moveInRevs, moveInRevs);
+    }
+
+    private void strafe(double strafeInches) {
+        // We assume that strafing right means positive
+        double strafeRevs = Math.abs(strafeInches / CIRCUMFERENCE_INCHES);
+        if (strafeInches >= 0) {
+            telemetry.addData("Strafing towards right by ", "%.3f inches", strafeInches);
+
+            drive(SPEED,
+                    1 * strafeRevs,
+                    -1 * strafeRevs,
+                    -1 * strafeRevs,
+                    1 * strafeRevs);
+        } else {
+            telemetry.addData("Strafing towards Left by ", "%.3f inches", Math.abs(strafeInches));
+
+            drive(SPEED,
+                    -1 * strafeRevs,
+                    1 * strafeRevs,
+                    1 * strafeRevs,
+                    -1 * strafeRevs);
+        }
+    }
+
+    public void rotate(double degrees) {
+
+        double robotSpeed = SPEED;
+        // Assume positive degrees means moving towards the right
+        double movementOfWheelsInRevs = Math.abs(degrees / DEGREES_MOTOR_MOVES_IN_1_REV);
+
+        if (degrees >= 0) {
+            drive(robotSpeed,
+                    1.0 * movementOfWheelsInRevs,
+                    1.0 * movementOfWheelsInRevs,
+                    -1 * movementOfWheelsInRevs,
+                    -1 * movementOfWheelsInRevs);
+        } else {
+            // Moving negative means rotating left
+            drive(robotSpeed,
+                    -1 * movementOfWheelsInRevs,
+                    -1 * movementOfWheelsInRevs,
+                    1.0 * movementOfWheelsInRevs,
+                    1.0 * movementOfWheelsInRevs);
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summerCamp/teleOp/CampTeleOp.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode.summerCamp.teleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.mmintothedeep.UtilityValues;
+
+@TeleOp(name="CURIE KICKOFF")
+public class CampTeleOp extends LinearOpMode {
+
+    DcMotor leftFrontDrive = null;
+    DcMotor rightFrontDrive = null;
+    DcMotor leftBackDrive = null;
+    DcMotor rightBackDrive = null;
+
+    /**
+     * Main section of code -- like 'main' method
+     * @throws InterruptedException - just in case
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize(); // initializing everything
+
+        waitForStart(); // waiting until driver clicks play button
+
+        while (opModeIsActive()) {
+
+            /*
+             * ===============
+             * YOUR CODE BELOW
+             * ===============
+             */
+
+            /*
+             * Hints:
+             * 1. set the x y and rx (rotation) based on the gamepad input
+             */
+
+            // you will need to change the 0s. they are placeholders.
+            double x = 0;
+            double y = 0; // hint, the y stick on the controller is reversed.
+            double rx = 0;
+
+            // Denominator is the largest motor power (abs value) or 1. It makes sure no more than 1 power is delivered.
+            // This makes sure that the ratio stays the same
+            // but only when at least one is out of range [-1, 1]
+            // the maximum (Math.max(value1, value2)) of either:
+            //      1. the sum of the absolute values (Math.abs(value)) of x, y, and rx
+            //      2. 1
+            // thix sets the denominator to the highest value if it is the sum or if it 1
+            double denominator = 0;
+            /*
+             * frontLeftPower should be the sum of y, x, and rx all divided by denominator
+             * backLeftPower should be y minus x plus rx all divided by denominator
+             * frontRightPower should be y minus x minus rx all divided by denominator
+             * backRightPower should be y plus x minus rx all divided by denominator
+             */
+            double frontLeftPower = 0;
+            double backLeftPower = 0;
+            double frontRightPower = 0;
+            double backRightPower = 0;
+
+            // set power to each of the motors now
+
+            
+
+            /*
+             * ===============
+             * YOUR CODE ENDS
+             * ===============
+             */
+
+        }
+
+    }
+
+    /**
+     * code to initialize everything
+     */
+    public void initialize() {
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftBackDrive.setDirection(UtilityValues.compLeftBackDirection);
+        leftFrontDrive.setDirection(UtilityValues.compLeftFrontDirection);
+        rightBackDrive.setDirection(UtilityValues.compRightBackDirection);
+        rightFrontDrive.setDirection(UtilityValues.compRightFrontDirection);
+
+    }
+
+
+}
