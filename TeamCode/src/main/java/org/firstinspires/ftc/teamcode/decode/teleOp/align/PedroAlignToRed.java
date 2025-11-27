34ac4e113b22ba15d23964da706e4d66e034dd22
srinandasai12 <srinandasai@gmail.com>
2025-11-27 16:16:44 -0500

    Update PedroAlignToRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/align/PedroAlignToRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/align/PedroAlignToRed.java
@@ -4,96 +4,104 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
-import com.pedropathing.paths.HeadingInterpolator;
-import com.pedropathing.paths.Path;
-import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.decode.pedroPathing.Constants;
 
-import java.util.function.Supplier;
 @Configurable
 @TeleOp
 public class PedroAlignToRed extends OpMode {
     private Follower follower;
-    public static Pose startingPose = new Pose(
-            116.6988847583643,
-            128.83271375464685,
-            Math.toRadians(225)
-    ); //See ExampleAuto to understand how to use this
-    private boolean automatedDrive;
-    private Supplier<PathChain> pathChain;
+    public static Pose startingPose;
+    private boolean automatedRotation;
     private TelemetryManager telemetryM;
     private boolean slowMode = false;
     private double slowModeMultiplier = 0.5;
+
+    // Fixed goal point on the field (adjust these coordinates as needed)
+    private static final double GOAL_X = 130.0;
+    private static final double GOAL_Y = 130.0;
+
     @Override
     public void init() {
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
         follower.update();
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
-        pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
-                .addPath(new Path(new BezierLine(follower::getPose, new Pose(100, 100))))
-                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
-                .build();
     }
+
     @Override
     public void start() {
-        //The parameter controls whether the Follower should use break mode on the motors (using it is recommended).
-        //In order to use float mode, add .useBrakeModeInTeleOp(true); to your Drivetrain Constants in Constant.java (for Mecanum)
-        //If you don't pass anything in, it uses the default (false)
         follower.startTeleopDrive();
     }
+
     @Override
     public void loop() {
-        //Call this once per loop
         follower.update();
         telemetryM.update();
-        if (!automatedDrive) {
-            //Make the last parameter false for field-centric
-            //In case the drivers want to use a "slowMode" you can scale the vectors
-            //This is the normal version to use in the TeleOp
-            if (!slowMode) follower.setTeleOpDrive(
-                    -gamepad1.left_stick_y,
-                    -gamepad1.left_stick_x,
-                    -gamepad1.right_stick_x,
-                    true // Robot Centric
-            );
-                //This is how it looks with slowMode on
-            else follower.setTeleOpDrive(
-                    -gamepad1.left_stick_y * slowModeMultiplier,
-                    -gamepad1.left_stick_x * slowModeMultiplier,
-                    -gamepad1.right_stick_x * slowModeMultiplier,
-                    true // Robot Centric
-            );
+
+        if (!automatedRotation) {
+            // Normal teleop driving
+            if (!slowMode) {
+                follower.setTeleOpDrive(
+                        -gamepad1.left_stick_y,
+                        -gamepad1.left_stick_x,
+                        -gamepad1.right_stick_x,
+                        true
+                );
+            } else {
+                follower.setTeleOpDrive(
+                        -gamepad1.left_stick_y * slowModeMultiplier,
+                        -gamepad1.left_stick_x * slowModeMultiplier,
+                        -gamepad1.right_stick_x * slowModeMultiplier,
+                        true
+                );
+            }
         }
-        //Automated PathFollowing
+
+        // Rotate to face goal point when A is pressed
         if (gamepad1.aWasPressed()) {
-            follower.followPath(pathChain.get());
-            automatedDrive = true;
+            double targetHeading = calculateHeadingToGoal();
+            follower.turnTo(targetHeading);
+            automatedRotation = true;
         }
-        //Stop automated following if the follower is done
-        if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
+
+        // Stop automated rotation if done or B is pressed
+        if (automatedRotation && (gamepad1.bWasPressed() || !follower.isBusy())) {
             follower.startTeleopDrive();
-            automatedDrive = false;
+            automatedRotation = false;
         }
-        //Slow Mode
+
+        // Slow Mode toggle
         if (gamepad1.rightBumperWasPressed()) {
             slowMode = !slowMode;
         }
-        //Optional way to change slow mode strength
+
+        // Slow mode strength adjustment
         if (gamepad1.xWasPressed()) {
             slowModeMultiplier += 0.25;
         }
-        //Optional way to change slow mode strength
         if (gamepad2.yWasPressed()) {
             slowModeMultiplier -= 0.25;
         }
+
+        // Telemetry
         telemetryM.debug("position", follower.getPose());
         telemetryM.debug("velocity", follower.getVelocity());
-        telemetryM.debug("automatedDrive", automatedDrive);
+        telemetryM.debug("automatedRotation", automatedRotation);
+        telemetryM.debug("targetHeading", Math.toDegrees(calculateHeadingToGoal()));
+        telemetryM.debug("goalPoint", "(" + GOAL_X + ", " + GOAL_Y + ")");
+    }
+
+    /**
+     * Calculate the heading angle needed to face the goal point from current position
+     */
+    private double calculateHeadingToGoal() {
+        Pose currentPose = follower.getPose();
+        double deltaX = GOAL_X - currentPose.getX();
+        double deltaY = GOAL_Y - currentPose.getY();
+        return Math.atan2(deltaY, deltaX);
     }
 }
