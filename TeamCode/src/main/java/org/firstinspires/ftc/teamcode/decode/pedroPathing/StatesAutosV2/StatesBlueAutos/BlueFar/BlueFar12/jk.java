5ba539c0487c763eabb3b3474f64276dbdff3ede
Nihalistheking <nihalbojji@gmail.com>
2026-02-03 19:05:17 -0500

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/pedroPathing/StatesAutosV2/StatesBlueAutos/BlueFar/BlueFar12/jk.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.decode.pedroPathing.StatesAutosV2.StatesBlueAutos.BlueFar.BlueFar12;
+
+public class jk {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/pedroPathing/StatesAutosV2/StatesRedAutos/RedClose/RedClose6/jk.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.decode.pedroPathing.StatesAutosV2.StatesRedAutos.RedClose.RedClose6;
+
+public class jk {
+}
