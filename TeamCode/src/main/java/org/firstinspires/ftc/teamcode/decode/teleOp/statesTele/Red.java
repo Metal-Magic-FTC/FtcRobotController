7cd6dde5821420b04bb87179cca6e876f1e3a8df
srinandasai12 <srinandasai@gmail.com>
2026-02-06 19:00:58 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/statesTele/Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/teleOp/statesTele/Red.java
@@ -1,12 +1,12 @@
-package org.firstinspires.ftc.teamcode.decode.teleOp.statesTele;
-
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.decode.pedroPathing.Alliance;
-
-@TeleOp(name="!!!!!! RedTele")
-public class Red extends Tele {
-    public Red() {
-        super(Alliance.RED);
-    }
-}
+//package org.firstinspires.ftc.teamcode.decode.teleOp.statesTele;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.teamcode.decode.pedroPathing.Alliance;
+//
+//@TeleOp(name="!!!!!! RedTele")
+//public class Red extends Tele {
+//    public Red() {
+//        super(Alliance.RED);
+//    }
+//}
