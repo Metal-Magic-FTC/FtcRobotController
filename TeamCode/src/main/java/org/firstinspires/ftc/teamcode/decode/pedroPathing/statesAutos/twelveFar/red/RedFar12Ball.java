40ca966fd24a96c639f20de262786e5c8234fcc9
Aryan Mathur <134450794+aryanmathur1@users.noreply.github.com>
2026-07-20 19:46:05 -0400

    Revert "Merge branch 'main' of https://github.com/Metal-Magic-FTC/FtcRobotController"
    This reverts commit ed48ddcdf30ba4b9711072c07cc4a4bab6b57f55, reversing
    changes made to b9e88d27aaa00e8a4f641f09abee0283141ce413.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/pedroPathing/statesAutos/twelveFar/red/RedFar12Ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/pedroPathing/statesAutos/twelveFar/red/RedFar12Ball.java
@@ -91,7 +91,7 @@ public class RedFar12Ball extends LinearOpMode {
         waitForStart();
         if (isStopRequested()) return;
 
-        launchMotor.setVelocity(3000);
+        launchMotor.setVelocity(1700);
 
         // scan balls
         //scanBallsInSlots(5000);
@@ -116,7 +116,7 @@ public class RedFar12Ball extends LinearOpMode {
         rotateToIndex(0);
         resetSlots();
 
-        // Intake 1
+        // ---- INTAKE 1–3 ----
         intakeActive = true;
         rotateToIndex(0);
         runPath(paths.toIntake1(), 0, 1);
@@ -124,16 +124,81 @@ public class RedFar12Ball extends LinearOpMode {
 
         runPathWithIntake(paths.intake1(), 0, 0.21);
         double startTime = System.currentTimeMillis();
-      
+//        while (System.currentTimeMillis() < startTime + 500) {
+//            waitingForBall = true;
+//            intakeActive = true;
+//            intake();
+//        }
+
+
+
+        runPathWithIntake(paths.gate(), 750, 1);
+
+        intakeMotor.setPower(-0.6);
+        slots[0] = Ball.PURPLE;
+        slots[1] = Ball.PURPLE;
+        slots[2] = Ball.GREEN;
+
+        aimToPattern(pattern);
+
+        slots[0] = Ball.PURPLE;
+        slots[1] = Ball.PURPLE;
+        slots[2] = Ball.GREEN;
+
+        runPath(paths.shoot2(), 0, 1);
+
+        // ---- SHOOT ----
+        //shootAllPattern(pattern);
+        shootAll();
+
+        intakeMotor.setPower(-0.6);
         intakeActive = true;
         rotateToIndex(0);
-        runPath(paths.shoot(), 0, 1);
         resetSlots();
 
+        // ---- INTAKE 4–6 ----
         intakeActive = true;
         rotateToIndex(0);
-        runPath(paths.wallBump(), 0, 1);
+        runPath(paths.toIntake2(), 0, 1);
+        runPathWithIntake(paths.intake2(), 0, 0.21);
+
+        intakeMotor.setPower(-0.6);
+        slots[0] = Ball.PURPLE;
+        slots[1] = Ball.GREEN;
+        slots[2] = Ball.PURPLE;
+
+        aimToPattern(pattern);
+
+        slots[0] = Ball.PURPLE;
+        slots[1] = Ball.GREEN;
+        slots[2] = Ball.PURPLE;
+
+        runPath(paths.shoot3(), 0, 1);
+
+        //shootAllPattern(pattern);
+        shootAll();
+
+        intakeMotor.setPower(-0.6);
+        intakeActive = true;
+        rotateToIndex(0);
         resetSlots();
+
+        runPath(paths.toIntake3(), 0, 1);
+        runPathWithIntake(paths.intake3(), 0, 0.21);
+
+        intakeMotor.setPower(-0.6);
+        slots[0] = Ball.GREEN;
+        slots[1] = Ball.PURPLE;
+        slots[2] = Ball.PURPLE;
+
+        aimToPattern(pattern);
+
+        slots[0] = Ball.GREEN;
+        slots[1] = Ball.PURPLE;
+        slots[2] = Ball.PURPLE;
+
+        runPath(paths.shoot4(), 0, 1);
+
         //shootAllPattern(pattern);
         shootAll();
 
