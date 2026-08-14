90debb77de92fb894409ea9524967af5e7f13d21
Aarush Gutha <aarushgutha@gmail.com>
2026-08-14 09:56:29 -0400

    sdfadda

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/biobuzz/slideTest.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.biobuzz;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@TeleOp(name="!!!!!!!TEST")
+public class slideTest extends OpMode {
+
+    private CRServo crServo;
+    private double amount;
+    @Override
+    public void init() {
+        crServo = hardwareMap.get(CRServo.class, "pivotServo");
+        crServo.setPower(0);
+        amount = 0.1;
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.x) {
+            amount += 0.01;
+        }
+        if (gamepad1.y) {
+            amount -= 0.01;
+        }
+        if (gamepad1.a) {
+            crServo.setPower(amount);
+        } else if (gamepad1.b) {
+            crServo.setPower(-amount);
+        } else {
+            crServo.setPower(0);
+        }
+        telemetry.addData("amt", amount);
+        telemetry.addData("power", crServo.getPower());
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/Curie/filledOutCurieTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/Curie/filledOutCurieTele.java
@@ -16,6 +16,10 @@ import com.qualcomm.robotcore.hardware.Servo;
  *   Motors: "leftFrontDrive", "rightFrontDrive", "leftBackDrive", "rightBackDrive"
  *   Servos: "pivotServo", "clawServo"
  *
+ *
+ * // NUMBER - PIVOT UP: 1      PIVOT DOWN: 0.88      CLAW_OPEN : 1     CLAW_CLOSE: 0
+ *
+ *
  * Controls:
  *   Left stick  -> drive forward/backward + strafe
  *   Right stick -> turn
@@ -26,20 +30,18 @@ import com.qualcomm.robotcore.hardware.Servo;
  * a line (or a few lines) of code yourself. Everything you need was covered
  * in the slides - look for the matching slide name in each TODO if you get stuck.
  */
-@TeleOp(name = "Basic TeleOp")
-public class BasicTele extends LinearOpMode {
+@TeleOp(name = "hopefully working good nice maybe good curie TeleOp")
+public class filledOutCurieTele extends LinearOpMode {
 
     // Declare our motors and servos. We connect them to the real
     // hardware later, inside initialize().
-    DcMotor leftFrontDrive;
-    DcMotor rightFrontDrive;
-    DcMotor leftBackDrive;
-    DcMotor rightBackDrive;
+    DcMotor leftFrontDrive = null;
+    DcMotor rightFrontDrive = null;
+    DcMotor leftBackDrive = null;
+    DcMotor rightBackDrive = null;
 
     Servo pivotServo = null;
     Servo clawServo = null;
-
-    boolean open = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -57,12 +59,13 @@ public class BasicTele extends LinearOpMode {
             // TODO 4 (slide "x, y, rx"): read gamepad1's left stick and right
             // stick into three variables named y, x, and rx.
             // Remember: y needs a minus sign in front of it!
-            double y = gamepad1.left_stick_x;
-            double x = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double y = -gamepad1.left_stick_y;
             double rx = gamepad1.right_stick_x;
+
             // TODO 5 (slide "Normalizing Motor Power"): create a variable called
             // denominator using Math.max(...) so no motor power ever goes above 1.0.
-            double denominator = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(rx),1);
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
             // TODO 6 (slide "Mecanum Wheel Math"): create the four power variables
             // - frontLeftPower, backLeftPower, frontRightPower, backRightPower -
@@ -70,10 +73,10 @@ public class BasicTele extends LinearOpMode {
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = ( y + x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
             // Max power. Anything lower will make sure the robot does not go fast, for safety
-            double motorSpeed = 0.4;
+            double motorSpeed = 1;
 
             // TODO 7 (slide "Setting power"): call setPower() on all four drive
             // motors, multiplying each power variable by motorSpeed.
@@ -82,25 +85,22 @@ public class BasicTele extends LinearOpMode {
             rightFrontDrive.setPower(frontRightPower * motorSpeed);
             rightBackDrive.setPower(backRightPower * motorSpeed);
 
-
-
             // Servos move to a POSITION (0.0 to 1.0), not a power like motors.
+
             // TODO 8 (slide "Servos"): if gamepad1.a is pressed, set the pivot
             // servo's position to 0.0. If gamepad1.b is pressed, set it to 1.0.
             if (gamepad1.a) {
-                pivotServo.setPosition(1);
-            }
-            if (gamepad1.b) {
-                pivotServo.setPosition(0.88);
+                pivotServo.setPosition(0.0);
+            } else if (gamepad1.b) {
+                pivotServo.setPosition(1.0);
             }
 
             // TODO 9 (slide "Servos"): do the same thing for the claw servo,
             // using gamepad1.x (closed, 0.0) and gamepad1.y (open, 1.0).
             if (gamepad1.x) {
-                clawServo.setPosition(0);
-            }
-            if (gamepad1.y) {
-                clawServo.setPosition(1);
+                clawServo.setPosition(0.0);
+            } else if (gamepad1.y) {
+                clawServo.setPosition(1.0);
             }
 
             telemetry.addData("Pivot Position", pivotServo.getPosition());
@@ -115,18 +115,15 @@ public class BasicTele extends LinearOpMode {
         // TODO 1 (slide "Declaring and Initializing Motors"): connect all four
         // motor variables to hardwareMap.get(DcMotor.class, "..."), using the
         // configuration names listed at the top of this file.
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFront");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBack");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
 
         // TODO 2 (slide "Servos"): connect pivotServo and clawServo to
         // hardwareMap.get(Servo.class, "...") the same way.
         pivotServo = hardwareMap.get(Servo.class, "pivotServo");
         clawServo = hardwareMap.get(Servo.class, "clawServo");
-
-        pivotServo.setPosition(0.86);
-        clawServo.setPosition(1);
 
         // Brake instead of coast when power is 0, for more control
         leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -138,10 +135,9 @@ public class BasicTele extends LinearOpMode {
         // motors using setDirection(DcMotorSimple.Direction...) - REVERSE the
         // left side motors, FORWARD the right side motors.
         leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotorSimple.Direction.FORWARD);
 
         telemetry.addLine("Ready! Press PLAY to start.");
         telemetry.update();
